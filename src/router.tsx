ca97c6980ceda8d9f5a5a60fe8ec2c26001b3a4a
DanRantino <danielaugustofurst@hotmail.com>
2026-01-20 23:39:47 -0300

    Hooks, extilos e pocketbase database

--- src/router.tsx
+++ src/router.tsx
@@ -1,17 +1,20 @@
-import { createRouter } from '@tanstack/react-router'
+import { createRouter } from "@tanstack/react-router";
 
 // Import the generated route tree
-import { routeTree } from './routeTree.gen'
+import { routeTree } from "./routeTree.gen";
+import { getServerAuthSnapshot } from "./lib/auth.ssr";
 
 // Create a new router instance
-export const getRouter = () => {
+export const getRouter = (request?: Request) => {
   const router = createRouter({
     routeTree,
-    context: {},
+    context: {
+      auth: null,
+    },
 
     scrollRestoration: true,
     defaultPreloadStaleTime: 0,
-  })
+  });
 
-  return router
-}
+  return router;
+};
